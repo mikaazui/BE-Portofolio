4224f2d21a88e40700c947e25694012c99247c39
mikaazui <adhika725@gmail.com>
2023-12-26 11:55:45 +0700

    adding more stuffs such as more comments and crating mre methods

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
+//middleware untuk collect data dari client
 app.use((req, res, next) => {
   let time = new Date()
   time = time.toLocaleDateString()
   const log = {
-    time: time,                                                                                                           
+    id: req.params,
+    time: time,
     path: req.path,
     method: req.method,
     ip: req.ip,
@@ -22,94 +23,128 @@ app.use((req, res, next) => {
 
   }
   console.info(log)
-  
+
   next()
 })
 
 app.get('/', (req, res) => {
-    res.send('<p>Hello World</p>')
-
-    res.format({
-        json: () => {
-          res.json({ message: 'Hello World JSON' })
-        }
-      })
-  })
-  
-app.get('/contact', (req, res) => {
-    // res.send('<p>Hello Contact</p>')
-    res.status(200).format({
-      json: () => {
-        res.send({
-          ip: req.ip,
-          query: req.query,
-          body: req.body,
-          path: req.path,
-          params: req.params
-          
-        })
-      },
-    })
-
-
-  })
-
-  app.post('/contact', (req, res) => {
-    res.send('<p>Hello Contact (post)</p>')
-  })
-
-  app.put('/contact/:category', (req, res) => {
-    // res.send('<p>Hello Contact (post)</p>')
-
-    res.status(200).format({
-      json: () => {
-        res.send({
-          ip: req.ip,
-          query: req.query,
-          body: req.body,
-          path: req.path,
-          params: req.params
-          
-        })
-      },
-    })
-  })
-  
-  app.patch('/contact', (req, res) => {
-    res.send('<p>Hello Contact (post)</p>')
-  })
-
-  app.delete('/contact', (req, res) => {
-    res.send('<p>Hello Contact (post)</p>')
-  })
-
-
-app.get('/about', (req, res) => {
+  res.send('<p>Hello World</p>')
   res.status(200).json({
-    message: 'berhasil masuk ke halaman about',
-  })})
+    message: 'berhasil masuk ke halaman home',
+  })
+})
+
+
+//separator
+
+
+//membaca data (contact)
+app.get('/contact/:id', (req, res) => {
+  // res.send('<p>Hello Contact</p>')
+  res.status(200).format({
+    json: () => {
+      res.send({
+        id: req.params.id,
+        ip: req.ip,
+        query: req.query,
+        body: req.body,
+        path: req.path,
+        params: req.params,
+        cookies: req.cookies,
+        protocol: req.protocol,
+
+      })
+    },
+  })
+
+
+})
+//membuat data contact (contact)
+app.post('/contact', (req, res) => {
+  res.status(200).json({
+    id: req.params.id,
+    ip: req.ip,
+    query: req.query,
+    body: req.body,
+    path: req.path,
+    params: req.params
+  })
+})
+//ubah data keseluruhan (contact)
+app.put('/contact/:id', (req, res) => {
+  res.status(200).json({
+    id: req.params.id,
+    ip: req.ip,
+    query: req.query,
+    body: req.body,
+    path: req.path,
+    params: req.params
+  })
+
+  res.status(200).json({
+    id: req.params.id,
+    ip: req.ip,
+    query: req.query,
+    body: req.body,
+    path: req.path,
+    params: req.params
+  })
+})
+//untuk mengubah sebagian data (contact)
+app.patch('/contact:id', (req, res) => {
+  res.status(200).json({
+    id: req.params.id,
+    ip: req.ip,
+    query: req.query,
+    body: req.body,
+    path: req.path,
+    params: req.params
+  })
+})
+//menghapus data (contact)
+app.delete('/contact/:id', (req, res) => {
+  res.status(200).json({
+    id: req.params.id,
+    ip: req.ip,
+    query: req.query,
+    body: req.body,
+    path: req.path,
+    params: req.params
+  })
+})
+
+
+//separator
+
 
 app.get('/project', (req, res) => {
   res.status(200).json({
     message: 'berhasil masuk ke halaman project',
   })
-  
-  })
+
+})
+
+
+// separator
 
 app.get('/blog', (req, res) => {
-    res.send('<p>Hello Blog</p>')
+  res.send('<p>Hello Blog</p>')
+})
+//separator
+app.post('/login', (req, res) => {
+
+  res.cookie('token', '12345')
+  res.cookie('name', 'valent')
+
+  res.status(200).json({
+    message: 'login success',
   })
 
-  app.post('/login', (req, res) => {
-    
-    res.cookie('token', '12345')
-    res.cookie('name', 'valent')
+})
 
-    res.status(200).json({
-      message: 'login success',
-    })
 
-  } )
+//separator
+
 
 app.delete('/logout', (req, res) => {
   res.clearCookie('token')
@@ -128,5 +163,5 @@ app.use((req, res) => {
 
 
 app.listen(5000, () => {
-    console.log("server running on http://localhost:5000");
+  console.log("server running on http://localhost:5000");
 })
