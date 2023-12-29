46da632452506fca8e8792e02ba0ca8757050d99
mikaazui <adhika725@gmail.com>
2023-12-29 11:37:36 +0700

    creating some more table

---
+++ prisma/migrations/20231229043548_create_table_skill/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `skill` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(100) NOT NULL,
+    `skill_category` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
