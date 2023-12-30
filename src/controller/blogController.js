7c2ac7c4e1efc769526bffd755f86734e4235b6a
mikaazui <adhika725@gmail.com>
2023-12-30 11:22:54 +0700

    fixed row = if (isNaN)

--- src/controller/blogController.js
+++ src/controller/blogController.js
@@ -13,86 +13,94 @@ const getAll = async (req, res) => {
 
 const get = async (req, res) => {
     try {
-    let id = req.params.id
-    id = parseInt(id)
-    const blog = await Prisma.blog.findUnique({
-        where: {
-            id
-        }
-    }) 
+        let id = req.params.id
+
+        // if (!Number(id)) {
+        //     return res.status(400).json({
+        //         message: "ID invalid"
+        //     }
+
+    if (isNaN(id)) {
+                return res.status(400).json({
+                    message: "ID invalid"
+                })
+            }
+
+            id = parseInt(id)
+            
+            const blog = await Prisma.blog.findUnique({
+                where: {
+                    id
+                }
+            })
 
 
-    //handle not found
-        if (blog == null) {
-            return res.status(404).json({
-                message: 'blog tidak ditemukan'
+            //handle not found
+            if (blog == null) {
+                return res.status(404).json({
+                    message: 'blog tidak ditemukan'
+                })
+            }
+
+
+
+            res.status(200).json({
+                message: 'berhasil masuk ke halaman blogs (berdasakan id)',
+                id: id,
+                blog: blog
+
+            });
+
+        } catch (error) {
+            res.status(500).json({
+                message: "server error :" + error.message
             })
         }
 
-        if(isNaN(id)){
-            res.status(400).json({
-                message: "ID invalid"
-            })
-        }
-        
-    
-        res.status(200).json({
-            message: 'berhasil masuk ke halaman blogs (berdasakan id)',
-            id: id,
-            blog: blog
-    
-        });
-        
-    } catch (error) {
-        res.status(500).json({
-            message: "server error :" + error.message
-        })
+
+
+
+
+
+
+
     }
 
 
-
-
-
-
-
-
-}
-
-
 const post = (req, res) => {
-    res.status(200).json({
-        message: 'berhasil masuk ke halaman blog',
-    })
+        res.status(200).json({
+            message: 'berhasil masuk ke halaman blog',
+        })
 
-}
+    }
 
-const put = (req, res) => {
-    res.status(200).json({
-        message: 'berhasil masuk ke halaman blog',
-    })
+    const put = (req, res) => {
+        res.status(200).json({
+            message: 'berhasil masuk ke halaman blog',
+        })
 
-}
+    }
 
-const patch = (req, res) => {
-    res.status(200).json({
-        message: 'berhasil masuk ke halaman blog',
-    })
+    const patch = (req, res) => {
+        res.status(200).json({
+            message: 'berhasil masuk ke halaman blog',
+        })
 
-}
+    }
 
-const remove = (req, res) => {
-    res.status(200).json({
-        message: 'berhasil masuk ke halaman blog',
-    })
+    const remove = (req, res) => {
+        res.status(200).json({
+            message: 'berhasil masuk ke halaman blog',
+        })
 
-}
+    }
 
-export default {
-    get,
-    getAll,
-    post,
-    put,
-    patch,
-    remove
+    export default {
+        get,
+        getAll,
+        post,
+        put,
+        patch,
+        remove
 
-}
+    }
