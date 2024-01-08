2287c8ac098ee776e26fa28ab20786f88e67614d
mikaazui <adhika725@gmail.com>
2024-01-08 09:55:18 +0700

    handle get category by id (skill)

--- src/controller/skillController.js
+++ src/controller/skillController.js
@@ -2,15 +2,16 @@ import { Prisma } from "../application/prisma.js"
 import { isSkill } from "../validation/skillValidation.js"
 import { Validate } from "../application/validate.js"
 import skillService from "../services/skillService.js"
+import { isID } from "../validation/mainValidation.js"
 
 const getAll = async (req, res, next) => {
   try {
     const data = await Prisma.skill.findMany({
       include: {
-          category: true
-        }
-      })
-      console.log(data)
+        category: true
+      }
+    })
+    console.log(data)
     res.status(200).json({
       message: 'berhasil masuk ke halaman skills',
       data: data
@@ -23,11 +24,35 @@ const getAll = async (req, res, next) => {
   }
 
 }
-const get = (req, res, next) => {
-  res.status(200).json({
-    message: 'berhasil masuk ke halaman skills',
-  })
-
+const get = async (req, res, next) => {
+  try {
+    let id = req.params.id
+    id = Validate(isID, id)
+  
+    const skill = await Prisma.skill.findUnique({
+      where: {
+        id
+      },
+      include: {
+        category: true
+      }
+    })
+  
+    //handle not found
+    if (skill == null) {
+      throw new ResponseError(404, `blog ${id} not found`)
+    }
+  
+    res.status(200).json({
+      message: 'berhasil masuk ke halaman skill (berdasakan id)',
+      id: id,
+      data: skill
+  
+    });
+  } catch (error) {
+    
+  }
+ 
 }
 
 const post = async (req, res, next) => {
