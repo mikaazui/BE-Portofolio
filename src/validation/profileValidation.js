a5a8d1dd53e5cf1a82a95a975f76c4e54691e59d
putera <rizjamiputera@gmail.com>
2024-01-22 10:36:06 +0700

    FIXME profileValidation.js

--- src/validation/profileValidation.js
+++ src/validation/profileValidation.js
@@ -1,7 +1,7 @@
 import joi from 'joi';
 import { isString100, isText, isURL, isString25 } from './mainValidation.js';
 
-
+// FIXME label
 const isProfile = joi.object({
     email: isString100.email().required().lowercase().label('Email'),
     firstName: isString100.required().label('Firstname'),
